22c7aa90b4cb1acf9bde5b818adda504f22e3286
arnavkshk <arnav22112003@gmail.com>
2023-11-05 00:12:20 +0530

    making e-kiraya

---
+++ src/Components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='navmain'>
+      <div className="mainnav">
+        <div className="home">HOME</div>
+        <div className="rent my-3">RENT</div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
